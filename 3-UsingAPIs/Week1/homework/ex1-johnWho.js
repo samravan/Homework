e66009db0866ae7e8d1bf34fc72fb3eaef1dc649
roq zob <roqzob@gmail.com>
2021-07-14 10:53:42 +0430

    ex1 solved

--- 3-UsingAPIs/Week1/homework/ex1-johnWho.js
+++ 3-UsingAPIs/Week1/homework/ex1-johnWho.js
@@ -1,4 +1,5 @@
 'use strict';
+
 /*------------------------------------------------------------------------------
 Rewrite this function, but replace the callback syntax with the Promise syntax:
 - Have the `getAnonName` function return a `new Promise`.
@@ -7,20 +8,19 @@ Rewrite this function, but replace the callback syntax with the Promise syntax:
   didn't pass in a first name!"
 ------------------------------------------------------------------------------*/
 // TODO see above
-const getAnonName = (firstName, callback) => {
-  setTimeout(() => {
-    if (!firstName) {
-      callback(new Error("You didn't pass in a first name!"));
-      return;
-    }
-
-    const fullName = `${firstName} Doe`;
-
-    callback(fullName);
-  }, 1000);
+const getAnonName = (firstName) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (!firstName) {
+        reject(new Error("You didn't pass in a first name!"));
+      } else {
+        resolve(`${firstName} Doe`);
+      }
+    }, 1000);
+  });
 };
 
-getAnonName('John', console.log);
+getAnonName('John').then(console.log);
 
 // ! Do not change or remove the code below
 module.exports = getAnonName;
